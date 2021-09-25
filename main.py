fdecbcdf1957d0b6d3eadf9cc53c73dba67de844
mrslv-bss <bassmiroslav@gmail.com>
2021-09-25 17:09:44 +0300

    pycodestyle | environ config/input file passed

--- main.py
+++ main.py
@@ -7,44 +7,46 @@ from marshmallow import Schema, fields, ValidationError, validate, INCLUDE
 
 # Config File
 if os.environ.get('HOME_CONFIGFILE') is None:
-    print("WARNING | Configuration file-variable is missing") 
+    print("WARNING | Configuration file-variable is missing")
     if os.name == 'nt':  # Windows
-        exp = 'setx HOME_CONFIGFILE "{}\configuration.json"'.format(os.getcwd())
-        subprocess.Popen(exp, shell=True).wait()
-        print("WARNING | New configuration.json variable created. Please, restart script!")
+        e = r'setx HOME_CONFIGFILE "{}\configuration.json"'.format(os.getcwd())
+        subprocess.Popen(e, shell=True).wait()
+        print("New configuration.json variable created. Please, restart me")
         print("Terminate script? Y/N")
         terminate = input("> ").upper()
         if terminate == 'Y':
             quit()
         elif terminate == 'N':
-            print("Environment variables changes will take effect after reload.")
+            print("Environment variable changes will take effect after reload")
         else:
             print("Incorrect input, terminate script :)")
             quit()
 else:
-    print("Configuration file variable successfully finded: "+os.environ.get('HOME_CONFIGFILE'))
+    print("Configuration file variable successfully finded: ")
+    print(os.environ.get('HOME_CONFIGFILE'))
 
 # Input File
 if os.environ.get('HOME_INPUTFILE') is None:
-    print("WARNING | Input file-variable is missing") 
+    print("WARNING | Input file-variable is missing")
     print("Enter your input file name (Example - data.txt):")
     INPUTFILE = input("> ")
     if os.name == 'nt':  # Windows
-        exp = 'setx HOME_INPUTFILE "{}\{}"'.format(os.getcwd(), INPUTFILE)
-        subprocess.Popen(exp, shell=True).wait()
-        print("WARNING | New input variable({}) created. Please, restart script!".format(INPUTFILE))
+        e = 'setx HOME_INPUTFILE "{}\\{}"'.format(os.getcwd(), INPUTFILE)
+        subprocess.Popen(e, shell=True).wait()
+        print("New input var {} created. Please, restart me".format(INPUTFILE))
         print("Terminate script? Y/N")
         terminate = input("> ").upper()
         if terminate == 'Y':
             quit()
         elif terminate == 'N':
-            print("Environment variables changes will take effect after reload.")
+            print("Environment variable changes will take effect after reload")
         else:
             print("Incorrect input, terminate script :)")
             quit()
 else:
-    print("Input file variable successfully finded: "+os.environ.get('HOME_INPUTFILE'))
-    
+    print("Input file variable successfully finded: ")
+    print(os.environ.get('HOME_INPUTFILE'))
+
 # Read configuration.json
 with open(os.environ.get('HOME_CONFIGFILE')) as f:
     data = json.load(f)
